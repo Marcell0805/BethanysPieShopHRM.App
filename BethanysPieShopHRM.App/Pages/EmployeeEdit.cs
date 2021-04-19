fd6ca4615dd8627146653de04c753dc41e0b227d
M@rcell <marcellvannniekerk@gmail.com>
2021-04-19 16:12:45 +0200

    Adding Edit part

---
+++ BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeEdit
+    {
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+        [Parameter]
+        public string EmployeeId { get; set; }
+        public Employee Employee { get; set; } = new Employee();
+
+    }
+}
